4ac6757971bb4e2aa63f31ab51d2cc9b99e4edca
Lucas Lopes <131997933+dinastiadoge@users.noreply.github.com>
2023-11-27 14:04:00 -0300

    Arquivos do projeto
    Upload de todos os arquivos do projeto de Desenvolvimento Web dos alunos: Ana Letícia, Fernanda Mendes, João Holanda e Lucas Lopes.

---
+++ Prototipo TrabalhoV10/SCRIPT/programacao.js
@@ -0,0 +1,65 @@
+function toggleJava() {
+  var javaDiv = document.querySelector(".topJava");
+  javaDiv.style.display =
+    javaDiv.style.display === "none" || javaDiv.style.display === ""
+      ? "block"
+      : "none";
+
+  var setaImg = document.querySelector(".seta");
+  setaImg.classList.toggle("virada");
+}
+
+function toggleJavaScript() {
+  var javaSDiv = document.querySelector(".topJavaScript");
+  javaSDiv.style.display =
+    javaSDiv.style.display === "none" || javaSDiv.style.display === ""
+      ? "block"
+      : "none";
+
+  var setaImg = document.querySelector(".seta");
+  setaImg.classList.toggle("virada");
+}
+
+function togglePython() {
+  var pythonDiv = document.querySelector(".topPython");
+  pythonDiv.style.display =
+    pythonDiv.style.display === "none" || pythonDiv.style.display === ""
+      ? "block"
+      : "none";
+
+  var setaImg = document.querySelector(".seta");
+  setaImg.classList.toggle("virada");
+}
+
+function togglePHP() {
+  var phpDiv = document.querySelector(".topPHP");
+  phpDiv.style.display =
+    phpDiv.style.display === "none" || phpDiv.style.display === ""
+      ? "block"
+      : "none";
+
+  var setaImg = document.querySelector(".seta");
+  setaImg.classList.toggle("virada");
+}
+
+function toggleCpp() {
+  var CppDiv = document.querySelector(".topCpp");
+  CppDiv.style.display =
+    CppDiv.style.display === "none" || CppDiv.style.display === ""
+      ? "block"
+      : "none";
+
+  var setaImg = document.querySelector(".seta");
+  setaImg.classList.toggle("virada");
+}
+
+function toggleRuby() {
+  var RubyDiv = document.querySelector(".topRuby");
+  RubyDiv.style.display =
+    RubyDiv.style.display === "none" || RubyDiv.style.display === ""
+      ? "block"
+      : "none";
+
+  var setaImg = document.querySelector(".seta");
+  setaImg.classList.toggle("virada");
+}
