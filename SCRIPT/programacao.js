aece57bc49c50efeb8c57b4e67b2860b3fc877fb
Ana Letícia de Souza Pinheiro <87092368+AnaSouzaPinheiro@users.noreply.github.com>
2023-11-28 11:32:20 -0300

    Update programacao.js

--- SCRIPT/programacao.js
+++ SCRIPT/programacao.js
@@ -9,17 +9,6 @@ function toggleJava() {
   setaImg.classList.toggle("virada");
 }
 
-function toggleJavaScript() {
-  var javaSDiv = document.querySelector(".topJavaScript");
-  javaSDiv.style.display =
-    javaSDiv.style.display === "none" || javaSDiv.style.display === ""
-      ? "block"
-      : "none";
-
-  var setaImg = document.querySelector(".seta");
-  setaImg.classList.toggle("virada");
-}
-
 function togglePython() {
   var pythonDiv = document.querySelector(".topPython");
   pythonDiv.style.display =
@@ -27,30 +16,8 @@ function togglePython() {
       ? "block"
       : "none";
 
-  var setaImg = document.querySelector(".seta");
-  setaImg.classList.toggle("virada");
-}
-
-function togglePHP() {
-  var phpDiv = document.querySelector(".topPHP");
-  phpDiv.style.display =
-    phpDiv.style.display === "none" || phpDiv.style.display === ""
-      ? "block"
-      : "none";
-
-  var setaImg = document.querySelector(".seta");
-  setaImg.classList.toggle("virada");
-}
-
-function toggleCpp() {
-  var CppDiv = document.querySelector(".topCpp");
-  CppDiv.style.display =
-    CppDiv.style.display === "none" || CppDiv.style.display === ""
-      ? "block"
-      : "none";
-
-  var setaImg = document.querySelector(".seta");
-  setaImg.classList.toggle("virada");
+  var setaImg1 = document.querySelector(".seta1");
+  setaImg1.classList.toggle("virada");
 }
 
 function toggleRuby() {
@@ -60,6 +27,39 @@ function toggleRuby() {
       ? "block"
       : "none";
 
-  var setaImg = document.querySelector(".seta");
-  setaImg.classList.toggle("virada");
+  var setaImg2 = document.querySelector(".seta2");
+  setaImg2.classList.toggle("virada");
+}
+
+function toggleCpp() {
+  var CppDiv = document.querySelector(".topCpp");
+  CppDiv.style.display =
+    CppDiv.style.display === "none" || CppDiv.style.display === ""
+      ? "block"
+      : "none";
+
+  var setaImg3 = document.querySelector(".seta3");
+  setaImg3.classList.toggle("virada");
+}
+
+function toggleJavaScript() {
+  var javaSDiv = document.querySelector(".topJavaScript");
+  javaSDiv.style.display =
+    javaSDiv.style.display === "none" || javaSDiv.style.display === ""
+      ? "block"
+      : "none";
+
+  var setaImg4 = document.querySelector(".seta4");
+  setaImg4.classList.toggle("virada");
+}
+
+function togglePHP() {
+  var phpDiv = document.querySelector(".topPHP");
+  phpDiv.style.display =
+    phpDiv.style.display === "none" || phpDiv.style.display === ""
+      ? "block"
+      : "none";
+
+  var setaImg5 = document.querySelector(".seta5");
+  setaImg5.classList.toggle("virada");
 }
